8826f63b6e9a0ab9f8b5b3fd311d30ecf2b7abc9
Goutham <m.n.goutham@gmail.com>
2019-04-09 22:08:25 -0400

    Fixes project template

--- cmd/location/location.go
+++
@@ -1,7 +0,0 @@
-package location
-
-import "fmt"
-
-func Location() {
-	fmt.Println("Welcome to Uber Location Service!")
-}
--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "location"
-
-func main() {
-
-	location.Location()
-}
